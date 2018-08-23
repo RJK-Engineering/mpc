56d49d8511385909ce9e1b43f5382f305ef760a6
RJK-Engineering <rjklinkhamer@gmail.com>
2018-08-23 14:00:31 +0100

    Removed dir, changed paths

--- mpcmon.bat
+++ mpcmon.bat
@@ -1,12 +1,11 @@
-@cd\
-@perl C:\scripts\mpc\mpcmon.pl ^
+@perl mpcmon.pl ^
 --run ^
 --port 13578 ^
 --window-title "%~n0" ^
---snapshot-dir "c:\data\mpc\snapshots" ^
---status-file "c:\data\mpc\.mpcmon\status.json" ^
---lock-file "c:\data\mpc\.mpcmon\lock" ^
---log-file "c:\data\mpc\.mpcmon\log" %*
+--snapshot-dir "p:\temp\mpc\snapshots" ^
+--status-file "p:\temp\mpc\.mpcmon\status.json" ^
+--lock-file "p:\temp\mpc\.mpcmon\lock" ^
+--log-file "p:\temp\mpc\.mpcmon\log" %*
 rem --after-move "dlc"
 rem --after-delete "..."
 
